88b2ba07fcca966d0e558fb6d21c09ff08164bf8
rohit@ROHIT <rohitsingh95724@gmail.com>
2025-07-30 12:40:37 +0530

    fixed server issue changed

--- Backend/index.js
+++ Backend/index.js
@@ -21,7 +21,7 @@ app.use(cors(corsOption));
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/",authRoute);
+app.use("/api/v1",authRoute);
 app.use("/api/v1",jobRoute)
 
 const PORT = process.env.PORT || 8000;
