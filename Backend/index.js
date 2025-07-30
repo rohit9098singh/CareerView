730709a631a1f01236da3b5486bb5e4fb7a168b3
rohit@ROHIT <rohitsingh95724@gmail.com>
2025-07-31 00:24:12 +0530

    helo

--- Backend/index.js
+++ Backend/index.js
@@ -21,7 +21,7 @@ const corsOption = {
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error("Not allowed by CORS"));
+      callback(new Error("Not allowed by CORS" ));
     }
   },
   credentials: true,
